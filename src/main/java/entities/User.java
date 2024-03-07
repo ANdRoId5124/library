be343859f656c06832b36bdc67336ebdf7bb86ed
Andrei Bychek <jkj43483@gmail.com>
2024-03-07 19:36:05 +0100

    application configurations

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,43 @@
+package entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long userId;
+
+  @Column
+  private String userName;
+
+  @Column
+  private String userSurname;
+
+  @Column
+  private String login;
+
+  @Column
+  private String password;
+
+  @ManyToOne
+  private Role role;
+
+  public User(String userName, String userSurname, String login, String password, Role role) {
+    this.userName = userName;
+    this.userSurname = userSurname;
+    this.login = login;
+    this.password = password;
+    this.role = role;
+  }
+}
