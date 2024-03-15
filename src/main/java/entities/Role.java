c4d50a1bba502c37079bea6b3be9c45d30760f31
Andrei Bychek <jkj43483@gmail.com>
2024-03-15 19:33:25 +0100

    Added documentation to the entities

--- src/main/java/entities/Role.java
+++ src/main/java/entities/Role.java
@@ -8,15 +8,25 @@ import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * The class that represents the roles of the user
+ */
 @Entity
 @Data
 @NoArgsConstructor
 public class Role {
 
+  /**
+   * The individual number of the role
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long roleId;
 
+  /**
+   * The role name
+   * For example: Admin, client
+   */
   @Column
   private String roleName;
 
