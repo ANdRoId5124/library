d8c4e645d9c06b817fcdfdda424d88616cf19367
Andrei Bychek <jkj43483@gmail.com>
2024-03-23 17:22:53 +0100

    Added custom hashcode and equals to book and rentedBook

--- src/main/java/com/andrei/LibraryManager/entities/Book.java
+++ src/main/java/com/andrei/LibraryManager/entities/Book.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -43,4 +44,22 @@ public class Book {
    */
   @Column
   private String author;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Book book = (Book) o;
+    return Objects.equals(title, book.title) && Objects.equals(author,
+        book.author);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(title, author);
+  }
 }
