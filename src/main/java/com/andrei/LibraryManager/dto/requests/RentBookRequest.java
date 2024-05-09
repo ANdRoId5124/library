f009fad22d0deffe55b54d05a93dd1a697b95fbe
Andrei Bychek <jkj43483@gmail.com>
2024-05-09 20:22:19 +0200

    moved to package dto.request

---
+++ src/main/java/com/andrei/LibraryManager/dto/requests/RentBookRequest.java
@@ -0,0 +1,24 @@
+package com.andrei.LibraryManager.dto.requests;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RentBookRequest {
+
+  @NotNull(message = "Email should not be null")
+  @Pattern(message = "Email is not valid", regexp =
+      "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+          + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
+  private String email;
+
+  @NotNull
+  private String title;
+  @NotNull
+  private String author;
+}
